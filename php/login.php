542d33794282c2561d672e4b641f2e26a7ab14f6
Andrew96-eng <u15191223@tuks.co.za>
2020-08-31 17:14:06 +0200

    Splash Page

---
+++ php/login.php
@@ -0,0 +1,40 @@
+<?php
+
+    error_reporting(E_ALL);
+    ini_set('error_reporting', E_ALL);
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "dbProject";
+    $mysqli = mysqli_connect($server, $username, $password, $database);
+
+    $email = isset($_POST["loginEmail"]) ? $_POST["loginEmail"] : false;
+    $pass = isset($_POST["loginPassword"]) ? $_POST["loginPassword"] : false;	
+
+    $sql = "SELECT * FROM tbusers WHERE email = '$email'";
+
+	$result = $mysqli->query($sql);
+
+	if($result && $result->num_rows > 0)
+	{
+        while($row = mysqli_fetch_assoc($result))
+		{
+            if($row["password"] == $password)
+            {
+                //successfull login
+                header("Location: html/homepage.html", true, 301);
+                exit();
+            }
+            else
+            {
+                // incorrect password
+            }
+        }
+    }
+    else
+    {
+        // Failed login
+    }
+
+?>
